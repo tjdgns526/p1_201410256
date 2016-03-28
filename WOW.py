934e1019ee43f12ae370bf6152ae62a2efcc3a21
tjdgns526 <gbch526@naver.com>
2016-03-28 12:25:27 +0900

    Added files via upload

---
+++ WOW.py
@@ -0,0 +1,21 @@
+﻿def Converttem():
+    cf=raw_input("Choose! celsius or fahrenheit: ")
+
+      if(cf=='c'):
+           print "You entered celsius, enter It's value"
+           temp=raw_input("celsius value: ")
+           fahtemp=float(temp)
+           tem=(fahtemp *1.8)+32
+           print tem
+      else:
+       print "You entered fahrenheit, enter it's value"
+       temp = raw_input("fahrenheit value: ")
+       celtemp=float(temp)
+       tem=(celtem-32)/1.8
+       print tem
+       
+
+Converttem()
+
+wn.exitonclick()
+
---
+++ dohyung.py
@@ -0,0 +1,33 @@
+﻿
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+width=t1.window_width()
+height=t1.window_height()
+width
+height
+def drawTriangleAt():
+ t1.penup()
+ t1.goto(-320,150)
+ t1.pendown()
+ t1.goto(-400,-150)
+ t1.goto(-240,-150)
+ t1.goto(-320,150)
+def drawPolygon():
+ t1.penup()
+ t1.goto(0,150)
+ t1.pendown()
+ t1.goto(-100,50)
+ t1.goto(-50,-150)
+ t1.goto(50,-150)
+ t1.goto(100,50)
+ t1.goto(0,150)
+def drawStar():
+ t1.penup()
+ t1.goto(320,150)
+ t1.pendown()
+ t1.goto(270,-150)
+ t1.goto(420,0)
+ t1.goto(220,0)
+ t1.goto(370,-150)
+ t1.goto(320,150)
