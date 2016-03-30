21ea95c0fc9245f7f6e1cabd60d4c3de16d5fca0
KooSeongHun-201410256 <gbch526@naver.com>
2016-03-30 15:48:49 +0900

    Added files via upload

---
+++ w5Main5.py
@@ -0,0 +1,22 @@
+﻿def ComputeGrade(Marks):
+    if (90<=Marks<=100):
+        grade= 'A'
+    elif(80<=Marks<90):
+        grade= 'B'
+    elif (70<=Marks<80):
+        grade= 'C'
+    elif (60<=Marks<70):
+        grade= 'D'
+    elif (Marks<60):
+        grade= 'F'
+    return grade
+
+MarksTmp = raw_input("Enter Your Marks(0~100):")
+Marks=float(MarksTmp)
+mygrade=ComputeGrade(Marks)
+print mygrade
+
+exitonclick()
+
+
+     
