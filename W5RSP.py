e6313c0eb212fa607fccd6a38e5d039f6b3a0a12
KooSeongHun-201410256 <gbch526@naver.com>
2016-04-02 22:46:31 +0900

    Added files via upload
    RockScissorPaper!

---
+++ W5RSP.py
@@ -0,0 +1,26 @@
+﻿User=raw_input("User Choose?(Scissor=1,Rock=2,Paper=3):")
+Com=raw_input("Computer Choose?(Scissor=1,Rock=2,Paper=3:)")
+
+def RSP():
+    if User=='1':
+        if Com=='1':
+            print "서로 비겼습니다."
+        if Com=='2':
+            print "컴퓨터가 이겼습니다."
+        if Com=='3':
+            print "내가 이겼습니다."
+    elif User=='2':
+        if Com=='1':
+            print "내가 이겼습니다."
+        if Com=='2':
+            print "서로 비겼습니다."
+        if Com=='3':
+            print "컴퓨터가 이겼습니다."
+    elif User=='3':
+        if Com=='1':
+            print "컴퓨터가 이겼습니다."
+        if Com=='2':
+            print "내가 이겼습니다."
+        if Com=='3':
+            print "서로 비겼습니다."
+RSP()
