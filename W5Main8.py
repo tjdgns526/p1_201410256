cae0eaccb09f566f0586957569e6f0596b269fa1
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-04 17:19:34 +0900

    BMI
    BMI Computing

---
+++ W5Main8.py
@@ -0,0 +1,17 @@
+﻿def ComputeBMI(height,weight):
+     BMI=weight/(height*height)
+     if(BMI<18.5):
+         res="LowWeight"
+     elif(18.5<=BMI<23):
+         res="Normal"
+     elif(23<=BMI<25):
+         res="OverWeight"
+     elif(25<=BMI<30):
+         res="Obesity"
+     elif(30<=BMI):
+         res="OverObesity"
+     else:
+         res="Error! Please just value"
+     return res, BMI
+
+ComputeBMI(1.8,80)
