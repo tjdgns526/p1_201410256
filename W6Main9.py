b0116a6a38efc47dd8125a23495069348db5b339
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-06 15:04:34 +0900

    sumOfMultiplesOf3_5
    sumOfMultiplesOf3_5

---
+++ W6Main9.py
@@ -0,0 +1,10 @@
+﻿def sumOfMultiplesOf3_5(begin,end):
+    sum=0
+    for i in range(begin,end):
+             if (i%3==0 or i%5==0):
+                 sum=sum+i
+             elif(i%15==0):
+                 sum=sum-i
+    return sum
+
+sumOfMultiplesOf3_5(1,1000)
