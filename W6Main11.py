c18d136371a760abb62119165b7e5a1c7a0e632a
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-06 15:30:15 +0900

    YoonNyeon
    YoonNyeon

---
+++ W6Main11.py
@@ -0,0 +1,9 @@
+﻿def FindingYoonNyeon(year):
+
+    if(year%4 == 0) and (year%100 !=0 or year%400==0):
+        res="YoonNyeon"
+    else:
+        res="PyeongNyeon"
+    return res
+
+FindingYoonNyeon(2016)
