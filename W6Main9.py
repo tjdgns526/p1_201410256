ee52acbf071d239e71e32a863c906926d97de554
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-06 15:04:08 +0900

    Delete W6Main9.py

--- W6Main9.py
+++
@@ -1,8 +0,0 @@
-﻿def sumOfMultiplesOf3_5(begin,end):
-    sum=0
-    for i in range(begin,end):
-             if (i%3==0 or i%5==0):
-                 sum=sum+i 
-    return sum
-
-sumOfMultiplesOf3_5(1,1000)
