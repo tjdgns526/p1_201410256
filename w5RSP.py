25bb508a802af5bdf14eed7787fd114adbcc21ff
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-13 22:15:22 +0900

    Update w5RSP.py

--- w5RSP.py
+++ w5RSP.py
@@ -4,23 +4,23 @@ Com=raw_input("Computer Choose?(Scissor=1,Rock=2,Paper=3:)")
 def RSP():
     if User=='1':
         if Com=='1':
-            print "서로 비겼습니다."
+            print "Draw."
         if Com=='2':
-            print "컴퓨터가 이겼습니다."
+            print "Computer Win"
         if Com=='3':
-            print "내가 이겼습니다."
+            print "User win."
     elif User=='2':
         if Com=='1':
-            print "내가 이겼습니다."
+            print "User win."
         if Com=='2':
-            print "서로 비겼습니다."
+            print "Draw."
         if Com=='3':
-            print "컴퓨터가 이겼습니다."
+            print "Computer Win."
     elif User=='3':
         if Com=='1':
-            print "컴퓨터가 이겼습니다."
+            print "Computer Win."
         if Com=='2':
-            print "내가 이겼습니다."
+            print "User win."
         if Com=='3':
-            print "서로 비겼습니다."
+            print "Draw."
 RSP()
