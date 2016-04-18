b75c2f6672fd1a0a385eae745a60b4ab335d18a9
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-18 13:52:18 +0900

    W7Main.py
    W7Main.py

---
+++ W7Main.py
@@ -0,0 +1,21 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def drawSquareAtSave(size,pos):
+    tracks=list()
+    t1.penup()
+    t1.setpos(pos)
+    t1.pendown()
+    for i in range(0,4):
+        t1.forward(size)
+        t1.left(90)
+        tracks.append(t1.pos())
+    print tracks
+drawSquareAtSave(100,(0,0))
+def lab7():
+    drawSquareAtSave(size,pos)
+    
+def main():
+         lab7()
+if __name__=="__main__":
+	main()
