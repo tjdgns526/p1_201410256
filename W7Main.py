421fcfcc7ae923b76c41b35af23556ca441aa5a4
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-18 23:43:48 +0900

    Update W7Main.py
    with assignment 2

--- W7Main.py
+++ W7Main.py
@@ -1,8 +1,8 @@
 ﻿import turtle
 wn=turtle.Screen()
 t1=turtle.Turtle()
+tracks=list()
 def drawSquareAtSave(size,pos):
-    tracks=list()
     t1.penup()
     t1.setpos(pos)
     t1.pendown()
@@ -10,10 +10,17 @@ def drawSquareAtSave(size,pos):
         t1.forward(size)
         t1.left(90)
         tracks.append(t1.pos())
-    print tracks
-drawSquareAtSave(100,(0,0))
+    return tracks
+def drawSquareFromList(tracks):
+    for i in range(4):
+        t1.goto(tracks[i])
 def lab7():
-    drawSquareAtSave(size,pos)
-    
+    drawSquareAtSave(100,(0,0))
+    t1.clear()
+    print tracks
+    drawSquareFromList(tracks)
 def main():
-         lab7()
+    lab7()
+
+if __name__=="__main__":
+    main()
