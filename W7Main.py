5703c9dc810e83bf35b89a32d7018799a382e8d7
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-04-20 16:57:04 +0900

    Update W7Main.py

--- W7Main.py
+++ W7Main.py
@@ -1,8 +1,8 @@
-﻿import turtle
-wn=turtle.Screen()
-t1=turtle.Turtle()
-tracks=list()
 def drawSquareAtSave(size,pos):
+    ﻿import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    tracks=list()
     t1.penup()
     t1.setpos(pos)
     t1.pendown()
@@ -14,11 +14,60 @@ def drawSquareAtSave(size,pos):
 def drawSquareFromList(tracks):
     for i in range(4):
         t1.goto(tracks[i])
+def saveTracks():
+    import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    t1.speed(1)
+    t1.penup()
+    tracks = list()
+    t1.goto(-400,300)
+    tracks.append(t1.pos())
+    t1.pendown()
+    t1.pencolor("Red")
+    t1.right(90)
+    t1.fd(400)
+    tracks.append(t1.pos())
+    t1.backward(150)
+    tracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    tracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(300)
+    tracks.append(t1.pos())
+    t1.back(150)
+    tracks.append(t1.pos())
+    t1.right(90)
+    t1.fd(300)
+    tracks.append(t1.pos())
+    t1.left(90)
+    t1.right(90)
+    t1.right(90)
+    t1.fd(200)
+    tracks.append(t1.pos())
+    t1.fd(300)
+    tracks.append(t1.pos())
+    t1.back(100)
+    tracks.append(t1.pos())
+    t1.left(90)
+    t1.fd(200)
+    tracks.append(t1.pos())
+    return tracks
+def replayTracks(myTrack):
+    import turtle
+    wn=turtle.Screen()
+    t1=turtle.Turtle()
+    for i in range(0,len(myTrack)):
+        t1.goto(myTrack[i])
+
 def lab7():
     drawSquareAtSave(100,(0,0))
     t1.clear()
     print tracks
     drawSquareFromList(tracks)
+    myTrack = saveTracks()
+    replayTracks(myTrack)
 def main():
     lab7()
 
