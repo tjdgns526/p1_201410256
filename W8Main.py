061f66ab1fa53d4eeaba90b30f20a7e5377bac79
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-05-02 13:49:21 +0900

    Added files via upload

---
+++ W8Main.py
@@ -0,0 +1,19 @@
+﻿def charCount(word):
+    d=dict()
+    for c in word:
+        if c not in d:
+            d[c]=1    
+        else:
+            d[c]=d[c]+1
+    return d
+
+def lab8():
+    word=raw_input("Enter the word.")
+    data=charCount(word)
+    print data
+
+def main():
+    lab8()
+    
+if __name__=="__main__":
+    main()
