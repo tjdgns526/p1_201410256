6c6ab0659e7dbaf01ed7999c2b2c85fdc982af8c
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-05-04 00:31:34 +0900

    Update W8Main.py

--- W8Main.py
+++ W8Main.py
@@ -1,4 +1,4 @@
-﻿def charCount(word):
+def charCount(word):
     d=dict()
     for c in word:
         if c not in d:
@@ -7,13 +7,33 @@
             d[c]=d[c]+1
     return d
 
+def countDigitsLetters(word):
+    d=dict()
+    d.update({'digit':0,'alpha':0})
+    for i in word:
+        if i.isdigit():
+            d['digit']=d['digit']+1
+        elif i.isalpha():
+            d['alpha']=d['alpha']+1
+    return d
+def graph(d):  
+    import matplotlib
+    import matplotlib.pyplot as pit
+    pit.bar(range(len(d)),d.values(),align='center')
+    pit.xticks(range(len(d)),list(d.keys()))
+    pit.show()
+
 def lab8():
     word=raw_input("Enter the word.")
-    data=charCount(word)
-    print data
-
+    data1=charCount(word)
+    print data1
+    data2=countDigitsLetters(word)
+    print data2
+    graph(data1)
+    graph(data2)
+    
 def main():
     lab8()
-    
+
 if __name__=="__main__":
     main()
