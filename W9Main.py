b3c989df624eecdb9ecaf4458808cad10e0d1e88
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-05-04 17:57:28 +0900

    Update W9Main.py

--- W9Main.py
+++ W9Main.py
@@ -22,15 +22,28 @@ def graph(d):
     pit.bar(range(len(d)),d.values(),align='center')
     pit.xticks(range(len(d)),list(d.keys()))
     pit.show()
+    
+def homeStuff(mine,yours):
+    print mine.union(yours)
+    print mine.intersection(yours)
+    print mine-yours
+    print yours-mine
 
 def lab8():
     word=raw_input("Enter the word.")
     data1=charCount(word)
     print data1
+    
     data2=countDigitsLetters(word)
     print data2
+   
+    
     graph(data1)
     graph(data2)
+   
+    mine={'TV', 'phone', 'camera', 'fridger', 'mixer', 'audio', 'cd player', 'light', 'computer', 'notebook', 'recorder'}
+    yours={'coffee machine', 'radio', 'camera', 'running machine', 'ramp', 'computer', 'notebook', 'recorder'}
+    homeStuff(mine,yours)
     
 def main():
     lab8()
