50ae1e51c286a4ea9014b63f030d917f72520b71
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-05-31 14:08:06 +0900

    Delete append.py

--- append.py
+++
@@ -1,9 +0,0 @@
-﻿try:
-    fout=open('python.txt','a')
-    fin=open('outputNumber','r')
-    for line in fin:
-        fout.write(line)
-    fout.close()
-    fin.close()
-except Exception as e:
-    print e
