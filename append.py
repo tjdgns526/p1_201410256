65ce006b0ac3fec54ebd99129de3aa9e0240a038
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-05-31 14:04:37 +0900

    W13main.py
    W13main.py

---
+++ append.py
@@ -0,0 +1,9 @@
+﻿try:
+    fout=open('python.txt','a')
+    fin=open('outputNumber','r')
+    for line in fin:
+        fout.write(line)
+    fout.close()
+    fin.close()
+except Exception as e:
+    print e
