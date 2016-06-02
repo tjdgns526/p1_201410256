a18d9c96a92eaa353ec27d5f8617437aad0c2bc0
KooSeongHun (p1-201410256) <gbch526@naver.com>
2016-06-02 15:11:27 +0900

    oo
    oo

---
+++ W13main2.py
@@ -0,0 +1,20 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        name="dog"
+        sound='mong mong'
+    def bark(self):
+        print self.sound
+
+
+class Sichu(Dog):
+    name='Sichu'
+    sound='si Chu!'
+
+class PitBul(Dog):
+    name='PitBul'
+    sound='pit~Bul!;
+
+dog2=Sichu("Mac")
+dog3=PitBul("Bill")
+dog2.bark()
+dog3.bark()
